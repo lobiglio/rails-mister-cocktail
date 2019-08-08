0a0aab1f3fef0ff0878ea0661e5629379e871e1c
letizia obiglio <letizia.obiglio@gmail.com>
2019-08-08 11:57:06 +0200

    Models association + validation done

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,6 @@
 class Dose < ApplicationRecord
+  validates :cocktail_id, uniqueness: { scope: :ingredient_id }
+  validates :description, :cocktail_id, :ingredient_id, presence: true
   belongs_to :cocktail
   belongs_to :ingredient
 end
